3d0d0d93721dc78d0673d9b535d9a869e7fc450c
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-04-07 01:18:30 -0600

    Tests passing on 1.8.7

--- spec/omniconfig/type/list_spec.rb
+++ spec/omniconfig/type/list_spec.rb
@@ -15,10 +15,6 @@ describe OmniConfig::Type::List do
     instance.should be_kind_of(OmniConfig::Type::Base)
   end
 
-  it "raises an error if the raw value is not an array" do
-    expect { instance.value("FOO") }.to raise_error(OmniConfig::TypeError)
-  end
-
   it "attempts to convert to array using `to_a` if it can" do
     instance.value(1..5).should == %W[1 2 3 4 5]
   end
